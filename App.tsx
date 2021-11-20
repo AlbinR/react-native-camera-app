e5e200b28865de0d27956c5e4f47e2fc9f830a64
AlbinR <albin@rybergs.net>
2021-11-20 16:38:18 +0100

    second commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert,
+  ImageBackground,
+  Image,
+} from "react-native";
+import { Camera } from "expo-camera";
+let camera: Camera;
 
 export default function App() {
+  const [startCamera, setStartCamera] = React.useState(false);
+
+  const __startCamera = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    if (status === "granted") {
+      // do something
+    } else {
+      Alert.alert("Access denied");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#fff",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {startCamera ? (
+          <Camera
+            style={{ flex: 1, width: "100%" }}
+            ref={(r) => {
+              camera = r;
+            }}
+          ></Camera>
+        ) : (
+          <TouchableOpacity
+            style={{
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: "#14274e",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              height: 40,
+            }}
+          >
+            <Text
+              style={{
+                color: "#fff",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Take picture
+            </Text>
+          </TouchableOpacity>
+        )}
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +74,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
